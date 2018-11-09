9bd4b6282a3c896df5ce17ea45d8bb4bc481bb1e
ryhoneydew <lucia.yang88@gmail.com>
2018-11-09 17:03:34 -0500

    Update-assign token to subscribers

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const {Room, User} = require('../db/models')
 const OpenTok = require('opentok')
+const opentok = new OpenTok(process.env.API_KEY, process.env.API_SECRET)
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -15,15 +16,17 @@ router.get('/', async (req, res, next) => {
 router.get('/:roomId', async (req, res, next) => {
   try {
     const room = await Room.findById(req.params.roomId)
-    res.json(room)
+    const subscriber = await User.findById(req.user.id)
+    let sessionId = room.sessionId
+    let token = opentok.generateToken(sessionId)
+    await subscriber.update({subscriberId: req.user.id, token})
+    res.json({room, subscriber})
   } catch (err) {
     next(err)
   }
 })
 
 router.post('/new', (req, res, next) => {
-  let opentok = new OpenTok(process.env.API_KEY, process.env.API_SECRET)
-  console.log('--------', req.body)
   opentok.createSession({mediaMode: 'routed'}, async (err, session) => {
     if (err) {
       console.log(err)
@@ -32,7 +35,7 @@ router.post('/new', (req, res, next) => {
     }
 
     const publisher = await User.findById(req.user.id)
-    console.log('Heyyyy Publisher', session.id)
+
     let sessionId = session.sessionId
     req.body.roomName = 'abc'
     req.body.sessionId = sessionId
@@ -42,10 +45,9 @@ router.post('/new', (req, res, next) => {
       tokenOptions.role = 'publisher'
       tokenOptions.data = `username=${publisher.name}`
       let token = opentok.generateToken(sessionId, tokenOptions)
-      console.log('Heyyyy token!', token)
-      console.log('Heyyyy user!', req.user.id)
+
       await publisher.update({token: token, publisherId: req.user.id})
-      console.log('Heyyyyyyy publisher!!!!!!', publisher)
+
       res.send({newRoom, publisher})
     } catch (err) {
       next(err)
